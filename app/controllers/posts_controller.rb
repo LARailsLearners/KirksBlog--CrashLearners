f953e477ed275500452c47ea42b0236185a896a5
Kirk <kkchu791@gmail.com>
2015-08-11 21:32:36 -0700

    add devise and users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @posts = Post.all.order('created_at DESC')
   end
@@ -40,7 +42,7 @@ end
     @post.destroy
     redirect_to root_path
   end
-  
+
   private
 
   def post_params
